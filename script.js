616437b2fb6bda5acb8ff9d4bb78381340f61a5a
callumtate <callum.tate@carleton.ca>
2023-11-29 17:32:38 -0500

    Start

--- script.js
+++ script.js
@@ -12,15 +12,37 @@
 // Constants
 const appID = "app";
 const headingText = "To do. To done. ✅";
+const create = "create";
+const remove = "remove";
+const form = document.getElementById("create");
+const btnd = document.getElementById("remove");
+
+contents = [];
 
 // DOM Elements
 let appContainer = document.getElementById(appID);
-
+create.addEventListener("add", formHandler);
 //
 // Functions
 //
 
 // Add a heading to the app container
+function formHandler(event) {
+  event.preventDefault();
+  const input = document.getElementById("create");
+  const value = input.value;
+  contents.push(value);
+  console.log(contents);
+  add(contents);
+}
+
+
+function add(contents) {
+  const list = document.createElement("ul");
+  const item = document.createElement("li");
+  list.appendChild(item);
+
+}
 function inititialise() {
   // If anything is wrong with the app container then end
   if (!appContainer) {
